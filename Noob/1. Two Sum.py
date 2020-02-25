ccc20a2d8014d0d039cda0095a02e7c45e7446d8
John Chen <johnchen77w@gmail.com>
2020-02-24 21:54:49 -0500

    Update 1. Two Sum.py

--- Noob/1. Two Sum.py
+++ Noob/1. Two Sum.py
@@ -1,1 +1,10 @@
-
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = dict()
+        size = len(nums)
+        for i in range(size):
+            diff = target - nums[i]
+            if diff in dic:
+                index = dic[diff]
+                return [index, i]
+            else:
+                dic[nums[i]] = i
